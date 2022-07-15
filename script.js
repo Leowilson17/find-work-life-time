6c67923b1822ad72a7ba83b43b46adff411beab3
leowilson <leobca2018@gmail.com>
2022-07-15 19:10:47 +0530

    add-some-codes.

--- script.js
+++ script.js
@@ -12,17 +12,21 @@ for (i = 1972; i <= 2006; i++) {
 var leap;
 function fun(x) {
     leap = (~~x);
-    //  console.log(leap);
+    // console.log(leap);
 }
+
 // create month option
-var month = document.createElement('select');
+var birthMonth;
 function monName(monthName) {
+    birthMonth = (~~monthName);
+    // console.log(~~birthMonth);
+
     // append days in month container
     var dayEle = document.getElementById('day-cont');
     // clear days in loop
     dayEle.innerHTML = "";
     var dayOption;
-    if (leap % 4 == 0 && monthName == 'feb') {
+    if (leap % 4 == 0 && monthName == 1) {
         for (i = 1; i <= 29; i++) {
             dayOption = document.createElement('option');
             dayOption.innerText = i;
@@ -31,7 +35,7 @@ function monName(monthName) {
         }
     }
 
-    else if (monthName == 'feb') {
+    else if (monthName == 1) {
         for (i = 1; i <= 28; i++) {
             dayOption = document.createElement('option');
             dayOption.innerText = i;
@@ -39,7 +43,7 @@ function monName(monthName) {
             // console.log(dayEle);
         }
     }
-    else if (monthName == 'jan' || monthName == "mar" || monthName == 'may' || monthName == 'july' || monthName == 'aug' || monthName == "oct" || monthName == 'dec') {
+    else if (monthName == 0 || monthName == 2 || monthName == 4 || monthName == 6 || monthName == 7 || monthName == 9 || monthName == 11) {
         for (i = 1; i <= 31; i++) {
             dayOption = document.createElement('option');
             dayOption.innerText = i;
@@ -47,7 +51,7 @@ function monName(monthName) {
             // console.log(dayEle); 
         }
     }
-    else if (monthName == 'apr' || monthName == "june" || monthName == "sep" || monthName == "nov") {
+    else if (monthName == 3 || monthName == 5 || monthName == 8 || monthName == 10) {
         for (i = 1; i <= 30; i++) {
             dayOption = document.createElement('option');
             dayOption.innerHTML = i;
@@ -56,4 +60,88 @@ function monName(monthName) {
         }
     }
 }
+
+// get days
+var birthDate;
+function days(date) {
+    birthDate = (~~date);
+    // console.log(birthDate);
+}
+
+// display year
+var displayRetireYear;
+displayRetireYear = document.querySelector('.show-year');
+
+// retierment age select box
+var retAge = document.querySelector('#retire-id');
+retAge.setAttribute('onchange','changeValue(this.value)');
+var retValue;
+// clear option
+    retAge.innerHTML = "";
+    for (i = 45; i <= 60; i++) {
+        retireAge = document.createElement('option');
+        retireAge.innerHTML = i;
+        retAge.appendChild(retireAge);
+        // console.log(i);
+    }
+ function changeValue(value) {
+    retValue = (~~value);
+    console.log(retValue);
+};
+
+// get result-div
+var result = document.querySelector('.result-div');
+// go-btn event
+var checkBtn = document.querySelector('#go-btn');
+checkBtn.addEventListener('click', function () {
+
+    // clear option
+    result.innerHTML = "";
+    
+    // Calculation
+    var retirementYear = leap + retValue;
+    console.log(retirementYear);
+    var presentYear = new Date();
+    var thisYear = presentYear.getFullYear();
+    var remainingYear = retirementYear - thisYear;
+    console.log(remainingYear);
+
+
+    // var date = new Date(leap,birthMonth,birthDate);
+    // console.log(date.getFullYear()); 
+
+    // PastYear Calculation
+    pastYears = thisYear - leap;
+    console.log(pastYears);
+
+    //  PastYear Indicator
+    var lostingYear;
+    for (i = 0; i < pastYears; i++) {
+        lostingYear = document.createElement('p');
+        lostingYear.setAttribute('class', 'box-1');
+        result.appendChild(lostingYear);
+    }
+
+    // Remaining Year Indicator
+    var remainingIndicator;
+    for (i = 0; i < remainingYear; i++) {
+        remainingIndicator = document.createElement('p');
+        remainingIndicator.setAttribute('class', 'box-2');
+        result.appendChild(remainingIndicator);
+    }
+    displayRetireYear.innerHTML = 'Retirement Year: ' + retirementYear;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
